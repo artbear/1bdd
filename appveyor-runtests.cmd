43994421a2b8644ec02f9386bbf24f74c4df97b3
Artur Ayukhanov <aartbear@gmail.com>
2016-12-03 14:53:15 +0300

    Добавил прогон фич #98

--- appveyor-runtests.cmd
+++ appveyor-runtests.cmd
@@ -1,8 +1,6 @@
 @chcp 65001
 
-rem "%ProgramFiles(x86)%\OneScript\bin\oscript.exe" -encoding=utf-8 testrunner.os -runall tests xddReportPath tests
 "%ProgramFiles(x86)%\OneScript\bin\oscript.exe" -encoding=utf-8 c:\projects\1testrunner\testrunner.os -runall tests xddReportPath tests
-REM "%ProgramFiles(x86)%\OneScript\bin\testrunner.bat" -runall tests xddReportPath tests
 
 @if %ERRORLEVEL%==2 GOTO good_exit
 @if %ERRORLEVEL%==0 GOTO good_exit
@@ -10,4 +8,13 @@ REM "%ProgramFiles(x86)%\OneScript\bin\testrunner.bat" -runall tests xddReportPa
 exit /B 1
 
 :good_exit
+
+"%ProgramFiles(x86)%\OneScript\bin\oscript.exe" -encoding=utf-8 src\bdd.os features/core -junit-out tests/bdd-log.xml
+
+@if %ERRORLEVEL%==2 GOTO good_exit_bdd
+@if %ERRORLEVEL%==0 GOTO good_exit_bdd
+
+exit /B 1
+
+:good_exit_bdd
 exit /B 0
